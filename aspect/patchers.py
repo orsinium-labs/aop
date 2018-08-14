2d5fdafc927f5a0525fe173995be3d9bc0ea171a
Gram <master_fess@mail.ru>
2018-08-14 18:40:28 +0300

    object patching

--- aspect/patchers.py
+++ aspect/patchers.py
@@ -1,17 +1,26 @@
 import sys
 
-from .advice import Advice
+from .aspect import Aspect
 
 
 def patch_object(aspect, advice):
-    pass
+    name = aspect.__name__
+    if not name.endswith('Aspect'):
+        name += 'Aspect'
+    return type(
+        name,
+        [Aspect, aspect],
+        {'_advice': advice},
+    )
 
 
 def patch_module(module, advice):
     for object_name in dir(module):
         if advice.targets.match(object_name):
-            new_object = patch_object(getattr(module, object_name), advice)
-            setattr(module, object_name, new_object)
+            source_object = getattr(module, object_name)
+            if isinstance(source_object, type):
+                new_object = patch_object(source_object, advice)
+                setattr(module, object_name, new_object)
 
 
 def patch_past(advice):
