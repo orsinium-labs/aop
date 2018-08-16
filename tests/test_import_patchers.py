f93ad8ec198d021e789e80c94cc0e6f644cd2794
Gram <master_fess@mail.ru>
2018-08-16 20:25:26 +0300

    test unpatching

--- tests/test_import_patchers.py
+++ tests/test_import_patchers.py
@@ -1,4 +1,5 @@
 import sys
+from types import ModuleType
 
 from aspect import import_patchers as patchers
 from aspect.module import AspectModule
@@ -10,3 +11,7 @@ def test_patch_past():
 
     patchers.patch_past()
     assert isinstance(sys.modules['textwrap'], AspectModule)
+
+    patchers.unpatch_past()
+    assert not isinstance(sys.modules['textwrap'], AspectModule)
+    assert isinstance(sys.modules['textwrap'], ModuleType)
