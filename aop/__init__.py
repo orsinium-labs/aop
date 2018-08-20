271f93009a565e7985036bb8c87c45e2c7bfa44d
Gram <master_fess@mail.ru>
2018-08-20 15:42:20 +0300

    +config for tools

--- aop/__init__.py
+++ aop/__init__.py
@@ -2,4 +2,4 @@ from .interface import enable, disable, register
 from .matcher import match
 from .checker import patched
 
-__all__ = ['enable', 'disable', 'register', 'match']
+__all__ = ['enable', 'disable', 'register', 'match', 'patched']
